be9890d662e6405f747e37ad500b88d5843d655f
domroon <haeusser-dominik@web.de>
2023-04-26 16:07:07 +0200

    implement LCD Display

--- ESP_Radio.ino
+++ ESP_Radio.ino
@@ -1,11 +1,13 @@
 #include <WiFi.h>
 #include <Audio.h>
+#include <LiquidCrystal_I2C.h>
 
 #define I2S_DOUT      26  // connect to DAC pin DIN
 #define I2S_BCLK      27  // connect to DAC pin BCK
 #define I2S_LRC       25  // connect to DAC pin LCK
 
 Audio audio;
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 char ssid[] = "AlphaCentauri";
 char password[] = "6ER6bXskskZ";
@@ -30,16 +32,43 @@ void audio_info(const char *info){
     Serial.print("info        "); Serial.println(info);
 }
 
+void audio_showstreamtitle(const char *info){
+    String Text = info;
+    lcd.setCursor(0,1);
+    lcd.print(Text.substring(0,16));
+}
+
+void audio_showstation(const char *info){
+    String Text = info;
+    lcd.setCursor(0,0);
+    lcd.print(Text.substring(0,16));
+}
+
 void menu_loop() {
-    if (millis() - menuMillis >= 1000) {
-        Serial.println("HALLO WELT :)");
-        menuMillis = millis();
+    if (millis() - menuMillis >= 10000) {
+        audio.connecttohost("http://st01.dlf.de/dlf/01/128/mp3/stream.mp3");
+        // Serial.println("HALLO WELT :)");
+        menuMillis = 0;
     }
 }
 
 void setup() {
+    lcd.init();                      
+    lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print("ESP Radio");
+    lcd.setCursor(0,1);
+    lcd.print("Connect to WLAN");
+
     menuMillis = millis();
     initWiFi();
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("ESP Radio");
+    lcd.setCursor(0,1);
+    lcd.print("Connected");
+    
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
     audio.setVolume(2);
     audio.connecttohost("http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3");
@@ -47,5 +76,5 @@ void setup() {
 
 void loop() {
     audio.loop();
-    // menu_loop();
+    menu_loop();
 }
